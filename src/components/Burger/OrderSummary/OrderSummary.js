1f62805b6af765805437e193425e5510af115036
Charpell <charpellumeh@gmail.com>
2018-04-14 14:41:38 +0100

    Implementing the Button Component to cancel or continue

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
+
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
     .map(igKey => {
@@ -20,6 +22,9 @@ const orderSummary = (props) => {
       {ingredientSummary}
     </ul>
     <p>Continue to checkout</p>
+
+    <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+    <Button btnType="Success" clicked={props.purchaseContinueHandler}>CONTINUE</Button>
   </Aux>
   )
 };
